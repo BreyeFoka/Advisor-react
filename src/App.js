7a09ddd452ff1bbe658a41382c4b173eb1a9a815
Breye Foka Lagloire <breyefokalagloire@gmail.com>
2024-07-01 12:05:08 +0300

    based last commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import axios from "axios"
+
+import './App.css'
+class App extends React.Component {
+    state = {advice:''}
+    fetchAdvice = () => {
+        axios.get("https://api.adviceslip.com/advice")
+        .then((response) => {
+            const {advice} = response.data.slip
+            this.setState({advice})
+        })
+        .catch((err)=>
+        console.log(err))
+    }
+    componentDidMount(){
+        this.fetchAdvice();
+    }
+    render(){
+        const {advice} = this.state
+        return(
+        <>
+        <div className="app">
+            <div className="card">
+                <h1 className="heading">{advice}</h1>
+                <button className="button" onClick={this.fetchAdvice}>
+                    <span>GIVE ME ADVICE!</span>
+                </button>
+            </div>
+        </div>
+        </>
+         
+        )
+    }
+}
+export default App
